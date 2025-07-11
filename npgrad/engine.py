17b81a0978759a5c102bc04dd2232a9fe7aeaf6e
Bryan Anenberg <anenbergb@gmail.com>
2025-07-11 16:37:20 -0700

    added support for multiply

--- npgrad/engine.py
+++ npgrad/engine.py
@@ -5,57 +5,61 @@ import numpy as np
 class Tensor:
     """
     stores a numpy tensor and its gradient
-    
+
     - only support floating point data type
     """
 
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op=""):
         if isinstance(data, np.ndarray):
             if data.dtype not in (np.float16, np.float32, np.float64):
                 raise ValueError(f"Unsupported data type: {data.dtype}. Only support float 16, 32, 64")
             self.data = data
         else:
             self.data = np.array(data, dtype=np.float64)
-    
+
         self.grad = np.zeros_like(self.data)
         # internal variables used for autograd graph construction
         self._backward = lambda: None
         self._prev = set(_children)
-        self._op = _op # the op that produced this node, for graphviz / debugging / etc
+        self._op = _op  # the op that produced this node, for graphviz / debugging / etc
 
     def __add__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
         # broadcast may be required
         # broadcast will add 1s to the left if to match shape
         # addition will fail and raise an error if broadcast shapes are incompatible
-        out = Tensor(self.data + other.data, (self, other), '+')
+        out = Tensor(self.data + other.data, (self, other), "+")
 
         # For each dimension where the shape is 1 or the dimension was missing,
         # we sum the gradient of the output along that dimension
         def _backward():
-            self.grad += reduce_grad(out.grad, self.data.shape) 
+            self.grad += reduce_grad(out.grad, self.data.shape)
             other.grad += reduce_grad(out.grad, other.data.shape)
+
         out._backward = _backward
 
         return out
 
     def __mul__(self, other):
         other = other if isinstance(other, Tensor) else Tensor(other)
-        out = Tensor(self.data * other.data, (self, other), '*')
+        out = Tensor(self.data * other.data, (self, other), "*")
 
+        # broadcast other.data * out.grad, then reduce to self.data.shape
         def _backward():
-            self.grad += other.data * out.grad
-            other.grad += self.data * out.grad
+            self.grad += reduce_grad(other.data * out.grad, self.data.shape)
+            other.grad += reduce_grad(self.data * out.grad, other.data.shape)
+
         out._backward = _backward
 
         return out
 
     def __pow__(self, other):
         assert isinstance(other, (int, float)), "only supporting int/float powers for now"
-        out = Tensor(self.data**other, (self,), f'**{other}')
+        out = Tensor(self.data**other, (self,), f"**{other}")
 
         def _backward():
-            self.grad += (other * self.data**(other-1)) * out.grad
+            self.grad += (other * self.data ** (other - 1)) * out.grad
+
         out._backward = _backward
 
         return out
@@ -64,7 +68,7 @@ class Tensor:
         if dim is None:
             dim = tuple(range(self.data.ndim))
         out_data = self.data.sum(axis=dim, keepdims=keepdim)
-        out = Tensor(out_data, (self,), 'sum')
+        out = Tensor(out_data, (self,), "sum")
 
         def _backward():
             # add back the collapsed dimensions
@@ -72,15 +76,17 @@ class Tensor:
             # broadcast to match self.data.shape
             broadcast_grad = np.broadcast_to(grad, self.data.shape)
             self.grad += broadcast_grad
+
         out._backward = _backward
 
         return out
 
     def relu(self):
-        out = Tensor(0 if self.data < 0 else self.data, (self,), 'ReLU')
+        out = Tensor(0 if self.data < 0 else self.data, (self,), "ReLU")
 
         def _backward():
             self.grad += (out.data > 0) * out.grad
+
         out._backward = _backward
 
         return out
@@ -91,12 +97,14 @@ class Tensor:
         # topological order all of the children in the graph
         topo = []
         visited = set()
+
         def build_topo(v):
             if v not in visited:
                 visited.add(v)
                 for child in v._prev:
                     build_topo(child)
                 topo.append(v)
+
         build_topo(self)
 
         # go one variable at a time and apply the chain rule to get its gradient
@@ -104,37 +112,37 @@ class Tensor:
         for v in reversed(topo):
             v._backward()
 
-    def __neg__(self): # -self
+    def __neg__(self):  # -self
         return self * -1
 
-    def __radd__(self, other): # other + self
+    def __radd__(self, other):  # other + self
         return self + other
 
-    def __sub__(self, other): # self - other
+    def __sub__(self, other):  # self - other
         return self + (-other)
 
-    def __rsub__(self, other): # other - self
+    def __rsub__(self, other):  # other - self
         return other + (-self)
 
-    def __rmul__(self, other): # other * self
+    def __rmul__(self, other):  # other * self
         return self * other
 
-    def __truediv__(self, other): # self / other
+    def __truediv__(self, other):  # self / other
         return self * other**-1
 
-    def __rtruediv__(self, other): # other / self
+    def __rtruediv__(self, other):  # other / self
         return other * self**-1
 
     def __repr__(self):
         return f"Tensor(data={self.data}, grad={self.grad})"
-    
+
     @property
     def shape(self):
         return self.data.shape
-    
+
     def numel(self):
         return self.data.size
-    
+
     def item(self):
         return self.data.item()
 
@@ -143,6 +151,7 @@ class Tensor:
 
     def zeros_like(self):
         return Tensor(np.zeros_like(self.data))
+
 
 def reduce_grad(grad_output, original_shape):
     """
--- test/test_arithmetic.py
+++ test/test_arithmetic.py
@@ -49,6 +49,86 @@ def test_sum_broadcast():
     assert np.allclose(tensor1.grad, tensor1pt.grad.numpy())
     assert np.allclose(tensor2.grad, tensor2pt.grad.numpy())
 
+def test_sum_broadcast2():
+    tensor1 = Tensor([[1,2],[3,4]])
+    tensor2 = 1 + tensor1 + 2
+    sum_np  = tensor2.sum()
+    sum_np.backward()
+
+    tensor1pt = torch.tensor([[1,2],[3,4]], requires_grad=True, dtype=torch.float64)
+    tensor2pt = 1 + tensor1pt + 2
+    sumpt  = tensor2pt.sum()
+    sumpt.backward()
+
+    assert sum_np.data == sumpt.detach().numpy()
+    assert np.allclose(tensor1.grad, tensor1pt.grad.numpy())
+
+def test_sub_broadcast():
+    tensor1 = Tensor([[1,2],[3,4]])
+    tensor2 = Tensor([2,1])
+    tensor3 = tensor1 - tensor2
+    sum_np  = tensor3.sum()
+    sum_np.backward()
+
+    tensor1pt = torch.tensor([[1,2],[3,4]], requires_grad=True, dtype=torch.float64)
+    tensor2pt = torch.tensor([2,1,], requires_grad=True, dtype=torch.float64)
+    tensor3pt = tensor1pt - tensor2pt
+    sumpt  = tensor3pt.sum()
+    sumpt.backward()
+
+    assert sum_np.data == sumpt.detach().numpy()
+    assert np.allclose(tensor1.grad, tensor1pt.grad.numpy())
+    assert np.allclose(tensor2.grad, tensor2pt.grad.numpy())
+
+
+def test_multiply():
+    tensor1 = Tensor([[1,2],[3,4]])
+    tensor2 = tensor1.ones_like()
+    tensor3 = tensor1 * tensor2
+    sum_np  = tensor3.sum()
+    sum_np.backward()
+
+    tensor1pt = torch.tensor([[1,2],[3,4]], requires_grad=True, dtype=torch.float64)
+    tensor2pt = torch.ones_like(tensor1pt, requires_grad=True)
+    tensor3pt = tensor1pt * tensor2pt
+    sumpt  = tensor3pt.sum()
+    sumpt.backward()
+
+    assert sum_np.data == sumpt.detach().numpy()
+    assert np.allclose(tensor1.grad, tensor1pt.grad.numpy())
+    assert np.allclose(tensor2.grad, tensor2pt.grad.numpy())
+
+def test_multiply_broadcast():
+    tensor1 = Tensor([[1,2],[3,4]])
+    tensor2 = Tensor([2,1])
+    tensor3 = tensor1 * tensor2
+    sum_np  = tensor3.sum()
+    sum_np.backward()
+
+    tensor1pt = torch.tensor([[1,2],[3,4]], requires_grad=True, dtype=torch.float64)
+    tensor2pt = torch.tensor([2,1,], requires_grad=True, dtype=torch.float64)
+    tensor3pt = tensor1pt * tensor2pt
+    sumpt  = tensor3pt.sum()
+    sumpt.backward()
+
+    assert sum_np.data == sumpt.detach().numpy()
+    assert np.allclose(tensor1.grad, tensor1pt.grad.numpy())
+    assert np.allclose(tensor2.grad, tensor2pt.grad.numpy())
+
+def test_multiply_broadcast2():
+    tensor1 = Tensor([[1,2],[3,4]])
+    tensor2 = 2 * tensor1 * 3
+    sum_np  = tensor2.sum()
+    sum_np.backward()
+
+    tensor1pt = torch.tensor([[1,2],[3,4]], requires_grad=True, dtype=torch.float64)
+    tensor2pt = 2 * tensor1pt * 3
+    sumpt  = tensor2pt.sum()
+    sumpt.backward()
+
+    assert sum_np.data == sumpt.detach().numpy()
+    assert np.allclose(tensor1.grad, tensor1pt.grad.numpy())
+
 
 # def test_sanity_check():
 
